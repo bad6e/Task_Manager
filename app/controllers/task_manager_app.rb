088238beaaab870277cf00b8ad854612dc329b3c
Bret Doucette <doucette.bret@gmail.com>
2015-08-25 18:09:41 -0600

    Added styling

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -4,8 +4,25 @@ require 'models/task'
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__),'..')
 
+  set :method_override, true
+
   get '/' do
     erb :dashboard
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
+  end
+
+  put "/tasks/:id" do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
   end
 
   get '/tasks' do
